d98cd372b3d08c2b2fac37618ced96c40b6ff4ea
psyhello <nar0gind@gmail.com>
2018-08-10 11:03:09 +0300

    some new fields
    added new fields to select>option and change "" in mail.php

--- mail.php
+++ mail.php
@@ -1,43 +1,87 @@
 <?php
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+$name =htmlspecialchars($_POST['name']);
+$email = htmlspecialchars($_POST['email']);
+$message = htmlspecialchars($_POST['message']);
+$org = htmlspecialchars($_POST['organization']);
 $subject = $_POST['subject'];
 $headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-/*echo $name . $email . $message . $subject;*/
+$headers .= "Content-type:text/plain;charset=UTF-8" . "\r\n";
 
 
 
 switch ($subject) {
 	case '1':
-	$to = 'svistunov@gne.gov.spb.ru';
+	$to = 'zaharov@gne.gov.spb.ru';
 	$subject = 'Вопрос с официального сайта';
-	$m = "$name \n $email \n $message";
-		mail($to,$subject , $message, $headers);
-		echo "$to . $subject . $m";
+	$m = "Организация:$org \r\n Имя:$name \r\n Почта:$email \r\n  Сообщение:$message";
+		if (mail($to,$subject , $m, $headers)) {
+				echo "Your message has been sent";
+			}
+			else{
+				echo "Произошла ошибка. Сообщение не отправлено";
+			}
+			//echo "$to . $subject . $m";
 		break;
 	case '2':
-	$to = 'mazaev@gne.gov.spb.ru';
+	$to = 'volkov@gne.gov.spb.ru';
 	$subject = 'Вопрос с официального сайта';
-	$m = "$name \n $email \n $message";
-		mail("$to,$subject , $message, $headers");
-		echo "$to . $subject . $m";
+	$m = "Организация:$org \r\n Имя:$name \r\n Почта:$email \r\n  Сообщение:$message";
+		if (mail($to,$subject , $m, $headers)) {
+				echo "Your message has been sent";
+			}
+			else{
+				echo "Произошла ошибка. Сообщение не отправлено";
+			}
+		//echo "$to . $subject . $m";
 		break;
 	case '3':
-	$to = 'chegotova@gne.gov.spb.ru';
+	$to = 'mazaev@gne.gov.spb.ru';
 	$subject = 'Вопрос с официального сайта';
-	$m = "$name \n $email \n $message";
-		mail("$to,$subject , $message, $headers");
-		echo "$to . $subject . $m";
+	$m = "Организация:$org \r\n Имя:$name \r\n Почта:$email \r\n  Сообщение:$message";
+		if (mail($to,$subject , $m, $headers)) {
+				
+				echo "Your message has been sent";
+			}
+			else{
+				echo "Произошла ошибка. Сообщение не отправлено";
+			}
+		//echo "$to . $subject . $m";
 		break;
 	case '4':
-	$to = 'techsupport@gne.gov.spb.ru';
+	$to = 'fridental@gne.gov.spb.ru';
 	$subject = 'Вопрос с официального сайта';
-	$m = "$name \n $email \n $message";
-		mail($to, $subject , $message, $headers);
-		echo "$to . $subject . $m";
+	$m = "Организация:$org \r\n Имя:$name \r\n Почта:$email \r\n  Сообщение:$message";
+		if (mail($to,$subject , $m, $headers)) {
+				echo "Your message has been sent";
+			}
+			else{
+				echo "Произошла ошибка. Сообщение не отправлено";
+			}
+		//echo "$to . $subject . $m";
+		break;
+	case '5':
+	$to = 'svistunov@gne.gov.spb.ru';
+	$subject = 'Вопрос с официального сайта';
+	$m = "Организация:$org \r\n Имя:$name \r\n Почта:$email \r\n  Сообщение:$message";
+		if (mail($to,$subject , $m, $headers)) {
+				echo "Your message has been sent";
+			}
+			else{
+				echo "Произошла ошибка. Сообщение не отправлено";
+			}
+
+		break;
+	case '6':
+	$to = 'chegotova@gne.gov.spb.ru';
+	$subject = 'Вопрос с официального сайта';
+	$m = "Организация:$org \r\n Имя:$name \r\n Почта:$email \r\n  Сообщение:$message";
+		if (mail($to,$subject , $m, $headers)) {
+				echo "Your message has been sent";
+			}
+			else{
+				echo "Произошла ошибка. Сообщение не отправлено";
+			}
 		break;
 }
 ?>
