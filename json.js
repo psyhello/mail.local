54c46f18d396b870bdf50d515cd3105ed94b8173
psyhello <nar0gind@gmail.com>
2018-08-16 12:54:06 +0300

    new function
    added nulifier after succes

--- json.js
+++ json.js
@@ -5,7 +5,11 @@
           url: 'mail.php',
           data: msg,
           success: function(data) {
+
             $('.result').html(data);
+            $( '#vopros' ).each(function(){
+    this.reset();
+});
           },
           error:  function(xhr, str){
       alert('Возникла ошибка: ' + xhr.responseCode);
